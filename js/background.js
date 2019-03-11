478d3c88053f6c9277f24f646887b37c1bf487ec
af8952 <44977231+af8952@users.noreply.github.com>
2019-03-11 22:20:00 +0000

    solutions added

--- js/background.js
+++ js/background.js
@@ -12,20 +12,20 @@ $( document ).ready(function(){
   $('h1').on('click', function () {
 
     // 1. log to the console that you have clicked the button
-
+    console.log("H1 clicked");
     // this selects a randon background from above and stores it as the variable 'randomBackground'
     var randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
     // this creates a string that css will understand to be a background url
     var imageUrl = "url('./images/" + randomBackground + ".jpg')"
-    
-    
-    // 2. using jquery, change the image url of the body background to the new one saved above
 
+
+    // 2. using jquery, change the image url of the body background to the new one saved above
+$("body").css("background-image", "url('https://cached/image etc')
     // EXTENSION TASK!
     // 3. Change the text of the 'span' to be that of the photographer
     //   (hint, you should re-use randomBackgroun)
     // Double points if you replace '-' between their names with a space
-
-    
-  }) 
+$("span").text(randomBackground);
+$("h1").css()
+  })
 }) // here endeth the function
